c7db0771a0d91658a53200e18079218e6cd2a5c1
Matt Hicks <matt@outr.com>
2017-01-13 13:40:39 -0600

    Minor update to better report error for missing cookie.

--- core/js/src/main/scala/org/hyperscala/util/Cookies.scala
+++ core/js/src/main/scala/org/hyperscala/util/Cookies.scala
@@ -3,8 +3,8 @@ package org.hyperscala.util
 import org.scalajs.dom._
 
 object Cookies {
-  lazy val map = document.cookie.split(';').map(s => s.splitAt(s.indexOf('='))).map(t => t._1 -> t._2.substring(1)).toMap
+  private lazy val map = document.cookie.split(';').map(s => s.splitAt(s.indexOf('='))).map(t => t._1 -> t._2.substring(1)).toMap
 
   def get(name: String): Option[String] = map.get(name)
-  def apply(name: String): String = map(name)
+  def apply(name: String): String = get(name).getOrElse(throw new RuntimeException(s"Unable to find '$name' in map: ${map.map(t => s"${t._1} = ${t._2}").mkString("[", ", ", "]")}, cookies: ${document.cookie}"))
 }
